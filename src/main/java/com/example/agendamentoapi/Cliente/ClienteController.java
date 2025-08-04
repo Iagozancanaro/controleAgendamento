e53247d7a641e5dd3a105d60cdaa723328d6e8ca
Iago Zancanaro <iagozancanaro@gmail.com>
2025-08-04 18:42:41 -0300

    feat: implementa o Controller nas camadas de Cliente e Serviço com endpoints de CRUD e regras de negocio

--- src/main/java/com/example/agendamentoapi/Cliente/ClienteController.java
+++ src/main/java/com/example/agendamentoapi/Cliente/ClienteController.java
@@ -1,15 +1,84 @@
 package com.example.agendamentoapi.Cliente;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/cliente")
 public class ClienteController {
 
-    @GetMapping("/boasvindas")
-    public String boasVindas() {
-        return "Outra msg de teste";
+    private final ClienteService clienteService;
+
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
     }
+
+    @PostMapping("/criar")
+    @Operation(summary = "Cria um novo cliente", description = "Rota cria um novo cliente e insere no banco de dados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Cliente criado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro na criação do cliente")
+    })
+    public ResponseEntity<String> criarCliente(@RequestBody ClienteDTO cliente) {
+        ClienteDTO novoCliente = clienteService.criarCliente(cliente);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Cliente criado com sucesso");
+    }
+
+    @GetMapping("/listar")
+    public List<ClienteDTO> listarClientes() {
+        return clienteService.listarClientes();
+    }
+
+    @GetMapping("/listar/{id}")
+    @Operation(summary = "Lista o Cliente por ID", description = "Rota para listar um cliente pelo seu ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Cliente encontrado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Cliente não encontrado")
+    })
+    public ResponseEntity<?> listarClientePorId(@PathVariable Long id) {
+
+        ClienteDTO cliente = clienteService.buscarClientePorId(id);
+        if (cliente != null) {
+            return ResponseEntity.ok(cliente);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Cliente com o ID: " + id + " não existente");
+        }
+    }
+
+    @PutMapping("/alterar/{id}")
+    @Operation(summary = "Altera o cliente", description = "Rota para alterar um cliente")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Cliente alterado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Cliente não encontrado, não foi possível alterar")
+    })
+    public ResponseEntity<?> atualizarCliente(@PathVariable Long id, @RequestBody ClienteDTO clienteAtualizado) {
+        ClienteDTO cliente = clienteService.atualizarCliente(id, clienteAtualizado);
+        if (cliente != null) {
+            return ResponseEntity.ok(cliente);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("O cliente que voce esta tentando atualizar não existe");
+        }
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<String> deletarClientePorId(@PathVariable Long id) {
+
+        if (clienteService.buscarClientePorId(id) != null) {
+            clienteService.deletarCliente(id);
+            return ResponseEntity.ok("Cliente com o ID " + id + " deletado com sucesso");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("O cliente com o ID " + id + " não foi encontrado");
+        }
+    }
+
 }
--- src/main/java/com/example/agendamentoapi/Servico/ServicoController.java
+++ src/main/java/com/example/agendamentoapi/Servico/ServicoController.java
@@ -1,15 +1,85 @@
 package com.example.agendamentoapi.Servico;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.agendamentoapi.Cliente.ClienteDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/servico")
 public class ServicoController {
 
-    @GetMapping("/boasvindas")
-    public String boasVindas() {
-        return "testando";
+    private final ServicoService servicoService;
+
+    public ServicoController(ServicoService servicoService) {
+        this.servicoService = servicoService;
+    }
+
+    @PostMapping("/criar")
+    @Operation(summary = "Cria um novo serviço", description = "Rota cria um novo serviço e inserir no banco de dados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Serviço criado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro na criação do serviço")
+    })
+    public ResponseEntity<String> criarServico(@RequestBody ServicoDTO servico) {
+        ServicoDTO novoServico = servicoService.criarServico(servico);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Serviço criado com sucesso");
+    }
+
+    @GetMapping("/listar")
+    public List<ServicoDTO> listarServiços() {
+        return servicoService.listarServicos();
+    }
+
+    @GetMapping("/listar/{id}")
+    @Operation(summary = "Lista o serviço por ID", description = "Rota para listar um serviço pelo seu ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Serviço encontrado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Serviço não encontrado")
+    })
+    public ResponseEntity<?> listarServicoPorId(@PathVariable Long id) {
+
+        ServicoDTO servico = servicoService.buscarServicoPorId(id);
+        if (servico != null) {
+            return ResponseEntity.ok(servico);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Serviço com o ID: " + id + " não existente");
+        }
+    }
+
+    @PutMapping("/alterar/{id}")
+    @Operation(summary = "Altera o serviço", description = "Rota para alterar um serviço")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Serviço alterado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Serviço não encontrado, não foi possível alterar")
+    })
+    public ResponseEntity<?> atualizarServico(@PathVariable Long id, @RequestBody ServicoDTO servicoAtualizado) {
+        ServicoDTO servico = servicoService.atualizarServico(id, servicoAtualizado);
+        if (servico != null) {
+            return ResponseEntity.ok(servico);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("O serviço que voce esta tentando atualizar não existe");
+        }
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<String> deletarServico(@PathVariable Long id) {
+
+        if (servicoService.buscarServicoPorId(id) != null) {
+            servicoService.deletarServico(id);
+            return ResponseEntity.ok("Serviço com o ID " + id + " deletado com sucesso");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("O serviço com o ID " + id + " não foi encontrado");
+        }
     }
 }
